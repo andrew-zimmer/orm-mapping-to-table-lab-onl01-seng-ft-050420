82095e834f2bb65b4fa2f991937b6518d56e9b2e
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-03 12:20:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,14 @@ class Student
   def self.create_table 
     sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, 
-      name TEXT, grade TEXT)
+      name TEXT, grade TEXT);
+      SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
       SQL
     DB[:conn].execute(sql)
   end 
